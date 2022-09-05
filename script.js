f8b44f298f1e56dd63978ce835f7d1da50c4c286
Riley Newhart <riley@Rileys-MBP.lan>
2022-09-05 19:04:54 -0400

    finished the js file and project is fully functional

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-1
+setInterval(setClock, 1000)
+
+const hourHand = document.getElementById('hour-hand')
+const minuteHand = document.getElementById('minute-hand')
+const secondHand = document.getElementById('second-hand')
+
+function setClock() {
+    const currentDate = new Date()
+    const secondsRatio = currentDate.getSeconds() / 60
+    const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
+    const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
+    setRotation(secondHand, secondsRatio)
+    setRotation(minuteHand, minutesRatio)
+    setRotation(hourHand, hoursRatio)
+}
+
+function setRotation(element, rotationRatio) {
+    element.style.setProperty('--rotation', rotationRatio * 360)
+}
+
+setClock()
